8573cbfcf8de7ff3a934aabc9fc768c1aa16f6fe
debanjan19974 <85027539+debanjan19974@users.noreply.github.com>
2025-01-14 23:41:22 +0530

    1st Commit 2025

---
+++ Age_Calculator.py
@@ -0,0 +1,4 @@
+Age= int(input("Hey how old are you?\n"))
+decades = Age//10
+years = Age%10
+print ("You are"+" "+ str (decades) + " "+ "Decades and " + str (years) +" " +"years old.")
---
+++ Temperature.py
@@ -0,0 +1,11 @@
+Temperature = int (input("Hey what i current tempaerure \n"))
+
+if Temperature >= 50:
+   print ("it's To hot")
+elif Temperature >= 35:
+	print("it's very good Weatether")
+elif Temperature <=20:
+	print("it's very cold out side")
+else:
+   print(" else it's warm")
+
