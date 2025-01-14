232e429737de81fe6ac701f687c57bfbcd55caf7
debanjan19974 <85027539+debanjan19974@users.noreply.github.com>
2025-01-15 00:08:03 +0530

    Addition (+) Subtraction(-),Division (/), Multiplication (*) in Python!

---
+++ Sum.py
@@ -0,0 +1,24 @@
+#How to Addition (+) Subtraction(-),Division (/), Multiplication (*) in Python!
+
+Number= 10
+Numbar2=2
+Number3 = 50
+
+
+# Addition (+)
+sum =(Number+Numbar2+Number3)
+print(sum)
+
+#  Subtraction(-)
+
+sub =(Number-Numbar2-Number3)
+print(sub)
+
+# Division (/)
+
+Div = (Number/Numbar2)
+print(Div)
+
+#Multiplication (*)
+Mul=(Number*Numbar2)
+print(Mul)
