e33fe7dd2a5dc0e85f190dda84ad7b475504b026
debanjan19974 <85027539+debanjan19974@users.noreply.github.com>
2025-01-16 23:49:49 +0530

    Commit 2

---
+++ Practice1.py
@@ -0,0 +1,25 @@
+#P1: Write a program to take inpute from user and print their sum.
+
+
+number1= int(input("Enter 1st number\n"))
+number2= int(input("Enter 2nd number\n"))
+
+print("Sum of number1 and number2 is = ",number1+number2)
+
+
+#P2: Write A program to take input from user of a square and print area.
+
+Side= float(input("Please Enter side of a square\n "))
+
+area=(Side*Side)
+
+print("Area of a square is = ", area)
+
+# P3: Write a program to find avg of floating numbers
+
+A=float(input("Enter your 1st number \n"))
+B=float(input("Enter your 2nd number\n"))
+
+AVG= (A+B)/2
+
+print("AVG of two number is = ", AVG)
---
+++ Type Conversion.py
@@ -0,0 +1,21 @@
+#Type Conversion
+
+a=20
+b=20.30
+
+print(a+b)
+
+#Type Casting
+
+val="20"
+val2=20.30
+print(type(val))
+print(type(val2))
+# print(val+val2)
+# you will get a error because String can't be added with flot or int.So you need to type cast.
+
+val3 = int("2")
+val4 = 20.30
+print(type(val3))
+print(type(val4))
+print(val4+val3)
